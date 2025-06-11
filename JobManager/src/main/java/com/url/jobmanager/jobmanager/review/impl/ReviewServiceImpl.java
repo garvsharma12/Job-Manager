383a721fc00a803204deb33bed2325879cef8870
garvsharma12 <garv.sharma200212@gmail.com>
2025-06-12 00:10:41 +0530

    fixes:review

--- JobManager/src/main/java/com/url/jobmanager/jobmanager/review/impl/ReviewServiceImpl.java
+++ JobManager/src/main/java/com/url/jobmanager/jobmanager/review/impl/ReviewServiceImpl.java
@@ -59,6 +59,7 @@ public class ReviewServiceImpl implements ReviewService {
             Review review  = reviewRepository.findById(reviewId).orElse(null);
             Company company = review.getCompany();
             company.getReviews().remove(review);
+            review.setCompany(null);
             companyService.updateCompany(company, companyId);
             return true;
         }
